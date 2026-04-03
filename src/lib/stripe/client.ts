027e5c57963e076fd5b09f6504ce3efbc1770536
Debasis Kar M4 <debasiskarm4@Debasiss-MacBook-Air.local>
2026-04-03 12:28:22 +0530

    fix: specify Stripe apiVersion explicitly

--- src/lib/stripe/client.ts
+++ src/lib/stripe/client.ts
@@ -3,4 +3,6 @@ import Stripe from "stripe";
 const key = process.env.STRIPE_SECRET_KEY;
 if (!key) throw new Error("STRIPE_SECRET_KEY is not set");
 
-export const stripe = new Stripe(key);
+export const stripe = new Stripe(key, {
+  apiVersion: "2025-02-24.acacia",
+});
